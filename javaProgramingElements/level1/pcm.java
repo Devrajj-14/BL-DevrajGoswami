7d4d58eb06b8f6fe0d763c20c60e623ae892c1fe
Devrajj-14 <dg9770@srmist.edu.in>
2026-01-28 17:19:29 +0530

    [Devraj]:completer level 1 and 2 of java programing elements

---
+++ javaProgramingElements/level1/pcm.java
@@ -0,0 +1,14 @@
+package javaProgramingElements.level1;
+
+public class pcm {
+
+        public static void main(String[] args) {
+            String name = "Sam";
+            int maths = 94;
+            int physics = 95;
+            int chemistry = 96;
+
+            int average = (maths + physics + chemistry) / 3;
+            System.out.println(name + " average mark in PCM is " + average);
+        }
+}
